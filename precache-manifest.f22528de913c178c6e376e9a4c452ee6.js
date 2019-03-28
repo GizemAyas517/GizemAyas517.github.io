f4e84414c8cf75a065dd8acbafa5cc3887c2a6fc
gizemayas <gayas@deloitte.com>
2019-03-28 14:34:02 +0300

    added build files

---
+++ precache-manifest.f22528de913c178c6e376e9a4c452ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d837c13d941f0e0674a",
+    "url": "/static/js/main.4f834ad9.chunk.js"
+  },
+  {
+    "revision": "83a91d71f3829baf5795",
+    "url": "/static/js/2.8acdcf19.chunk.js"
+  },
+  {
+    "revision": "9d837c13d941f0e0674a",
+    "url": "/static/css/main.39f39525.chunk.css"
+  },
+  {
+    "revision": "a862a8ac382fd0d073f53e52406f74c3",
+    "url": "/index.html"
+  }
+];
